45262ea783947026ca2afd87dd56b0501b5cebf1
ciju cherian <ciju.ch3rian@gmail.com>
2011-09-05 01:47:43 +0530

    code cleanup and npm based install and adding keyboard shortcut

--- upload.js
+++ upload.js
@@ -1,9 +1,20 @@
+#!/usr/bin/env node
 var formidable = require('formidable'),
     path = require('path'),
     fs = require('fs'),
     http = require('http'),
     walk = require('walk'),
     sys = require('sys');
+
+var exec = require("child_process").exec;
+
+var lib  = path.dirname(fs.realpathSync(__filename));
+var cwd = fs.realpathSync(process.cwd());
+
+var child;
+
+var cmd = path.join(lib, "chrome-linux/chrome --debug-devtools-frontend=" + path.join(lib, "devtools"));
+
 
 // http://stackoverflow.com/questions/4980243/how-to-copy-a-file
 function update_original_with(original, updated) {
@@ -20,15 +31,15 @@ function update_original_with(original, updated) {
     
 }
 
+
 http.createServer(function(req, res) {
-    var cwd = process.cwd();
     
     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
         // parse a file upload
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             res.writeHead(204, {'content-type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
-            sys.debug('recieved: ');
+            // sys.debug('recieved: ');
             sys.debug(sys.inspect({fileds: fields, files: files}));
             // sys.debug('path orig: ' + path.join(cwd, fields['path']));
             // sys.debug(' from: '+ files['blob']['path']);
@@ -38,9 +49,9 @@ http.createServer(function(req, res) {
         return;
     }
 
-}).listen(8888);
+}).listen(9888);
 
-
+// watches for any change in the localhost files, and runs do_fn on it.
 function watch(dir, do_fn) {
     var options,
         walker;
@@ -80,25 +91,32 @@ function watch(dir, do_fn) {
 }
 
 
-var io = require('socket.io').listen(8889);
 
+// connect with websocket and send refresh command to the page.
 var cb = null;
+var io = require('socket.io').listen(9889);
 
 io.sockets.on('connection', function (socket) {
-    sys.debug('asigning');
     cb = function () {
-        sys.debug('called by watcher');
+        sys.log('called by watcher');
         socket.emit('refresh');
     };
-    sys.debug(cb);
 });
+
+var watch_path = process.argv[2] ? fs.realpathSync(process.argv[2]) : cwd;
 
 // for jeykll based site use "path.join(process.cwd(), '_site')"
-watch(process.cwd(), function (p, n) {
-    if (cb) {
-        cb();
-    }
+watch(watch_path, function (p, n) {
+    sys.log("path: " + watch_path);
+    if (cb) { cb(); }
 });
 
 
 
+child = exec(cmd, function (error, stout, sterr) {
+    sys.log("stout: " + stout);
+    sys.log("sterr: " + sterr);
+    if (error != null) {
+        sys.log("error: " + error);
+    }
+});
