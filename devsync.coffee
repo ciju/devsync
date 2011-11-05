5bbf8ff0c80b6cdc20d83dd75aa905ed27627648
ciju cherian <ciju.ch3rian@gmail.com>
2011-11-05 14:56:35 +0530

    refactor upload and refresh servers as class. stop on browser close

--- devsync.coffee
+++ devsync.coffee
@@ -12,62 +12,71 @@ log = (args...) ->
     st = e.stack.split('\n')[2].replace(/\ */, ' ').replace(' at ', '').replace(__dirname+'/', '')
     console.log.apply null, [st].concat args
 
-start_upload_server = (port, cwd) ->
-  # http://stackoverflow.com/questions/4980243/how-to-copy-a-file
-  update_local_file = (orig, updt) ->
-    orig_f = fs.createWriteStream orig
-    updt_f = fs.createReadStream updt
-    updt_f.addListener "data", (chunk) -> orig_f.write(chunk)
-    updt_f.addListener "close", -> orig_f.end()
 
-  http.createServer((req, res) ->
-    if req.url == '/upload' and req.method.toLowerCase() == 'post'
-      # parse a file upload
-      form = new formidable.IncomingForm()
-      form.parse req, (err, fields, files) ->
-        res.writeHead(204, {'content-type': 'text/plain', 'Access-Control-Allow-Origin': '*'})
-        # sys.debug(sys.inspect({fields: fields, files: files}))
-        update_local_file(path.join(cwd, fields['path']), files['blob']['path'])
-        res.end()
-  ).listen port
+class UploadServer
+  constructor: (port, cwd) ->
+    # http://stackoverflow.com/questions/4980243/how-to-copy-a-file
+    update_local_file = (orig, updt) ->
+      orig_f = fs.createWriteStream orig
+      updt_f = fs.createReadStream updt
+      updt_f.addListener "data", (chunk) -> orig_f.write(chunk)
+      updt_f.addListener "close", -> orig_f.end()
 
+    @server = http.createServer((req, res) ->
+      if req.url == '/upload' and req.method.toLowerCase() == 'post'
+        # parse a file upload
+        form = new formidable.IncomingForm()
+        form.parse req, (err, fields, files) ->
+          res.writeHead(204, {'content-type': 'text/plain', 'Access-Control-Allow-Origin': '*'})
+          # sys.debug(sys.inspect({fields: fields, files: files}))
+          update_local_file(path.join(cwd, fields['path']), files['blob']['path'])
+          res.end()
+    )
+    @server.listen port
+    log 'upload: started upload server'
 
-start_refresh_server = (port, wpath) ->
-  watch = (dir, fn) ->
-    walker = walk.walk(dir, {})
+  close: ->
+    @server.close()
+    log 'upload: closed upload server'
 
-    walker.on "names", (root, nodeNames) ->
-      nodeNames.sort (a, b) -> if a==b then 0 else (if a>b then 1 else -1)
+class RefreshServer
+  constructor: (port, wpath) ->
+    watch = (dir, fn) ->
+      walker = walk.walk(dir, {})
 
-    walker.on "file", (root, fileStats, next) ->
-      fs.watchFile(path.join(root, fileStats.name), fn)
-      next()
+      walker.on "names", (root, nodeNames) ->
+        nodeNames.sort (a, b) -> if a==b then 0 else (if a>b then 1 else -1)
 
-    walker.on "directories",  (root, dirStatsArray, next) -> next()
-    walker.on "errors",  (root, nodeStatsArray, next) -> next()
-    walker.on "end", -> log("all done")
+      walker.on "file", (root, fileStats, next) ->
+        fs.watchFile(path.join(root, fileStats.name), fn)
+        next()
 
-  # start the refresh connection
-  io = socket.listen port
-  global.cb = null
+      walker.on "directories",  (root, dirStatsArray, next) -> next()
+      walker.on "errors",  (root, nodeStatsArray, next) -> next()
+      walker.on "end", -> log("refresh: closed refresh server")
 
-  io.sockets.on 'connection', (socket) ->
-    log 'registering refresh socket'
-    global.cb = ->
-      log 'called by watcher'
-      socket.emit 'refresh'
+    # start the refresh connection
+    io = socket.listen port
+    global.cb = null
 
-  watch wpath, ->
-    log 'path: '+ wpath
-    cb?()
+    io.sockets.on 'connection', (socket) ->
+      log 'refresh: registering refresh socket'
+      global.cb = ->
+        log 'refresh: refresh request'
+        socket.emit 'refresh'
+
+    watch wpath, ->
+      log 'path: '+ wpath
+      cb?()
 
 
 exec_cmd = (cmd, opts) ->
   log 'cmd: ', cmd, ' opts: ', opts
   child = child_process.spawn cmd, opts, {cwd: __dirname}
-  child.stdout.on('data', (data) -> process.stdout.write(data))
-  child.stderr.on('data', (data) -> process.stderr.write(data))
-  child.on('exit', -> log 'exiting')
+  child.stdout.on 'data', (data) -> process.stdout.write(data)
+  child.stderr.on 'data', (data) -> process.stderr.write(data)
+  child.on 'exit', ->
+    process.kill process.pid, 'SIGINT'
 
 handle_chromium = (argv) ->
   opts = []
@@ -80,8 +89,7 @@ handle_chromium = (argv) ->
   if argv.l?                    #local: for testing (see the shell script)
     opts.push("-t")
 
-  chromium_path = path.join(__dirname, "devsync-chromium")
-  log 'options: ', chromium_path, opts
+  log 'options: ', opts
   exec_cmd "devsync-chromium", opts
 
 
@@ -93,8 +101,8 @@ module.exports.devsync = ->
   global.debug = argv.v?
 
   if not argv.r?      #if remove chromium, then no need to run servers
-    start_upload_server(9888, cwd)
-    start_refresh_server(9889, watch_path)
+    ups = new UploadServer 9888, cwd
+    new RefreshServer 9889, watch_path
 
   process.on 'uncaughtException', ->
     log 'uncaughtException: Blame it on ciju.ch3rian@gmail.com, some details would be appreciated.'
@@ -102,8 +110,8 @@ module.exports.devsync = ->
   process.on 'SIGTERM', ->
     log 'termination signal. do stuff'
     process.exit()
-  process.on 'SIGINT', ->
-    log '---------xxxxxxxxx'
+  process.on 'SIGINT', =>
+    ups?.close()
     process.exit()
 
   log 'setting up chromium: please wait'
--- devsync.js
+++ devsync.js
@@ -1,5 +1,5 @@
-var exec_cmd, handle_chromium, id, log, name, start_refresh_server, start_upload_server, _ref, _ref2;
-var __slice = Array.prototype.slice;
+var RefreshServer, UploadServer, exec_cmd, handle_chromium, id, log, name, _ref, _ref2;
+var __slice = Array.prototype.slice, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 _ref = {
   'formidable': 'formidable',
   'path': 'path',
@@ -27,80 +27,92 @@ log = function() {
     return console.log.apply(null, [st].concat(args));
   }
 };
-start_upload_server = function(port, cwd) {
-  var update_local_file;
-  update_local_file = function(orig, updt) {
-    var orig_f, updt_f;
-    orig_f = fs.createWriteStream(orig);
-    updt_f = fs.createReadStream(updt);
-    updt_f.addListener("data", function(chunk) {
-      return orig_f.write(chunk);
-    });
-    return updt_f.addListener("close", function() {
-      return orig_f.end();
-    });
-  };
-  return http.createServer(function(req, res) {
-    var form;
-    if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
-      form = new formidable.IncomingForm();
-      return form.parse(req, function(err, fields, files) {
-        res.writeHead(204, {
-          'content-type': 'text/plain',
-          'Access-Control-Allow-Origin': '*'
-        });
-        update_local_file(path.join(cwd, fields['path']), files['blob']['path']);
-        return res.end();
+UploadServer = (function() {
+  function UploadServer(port, cwd) {
+    var update_local_file;
+    update_local_file = function(orig, updt) {
+      var orig_f, updt_f;
+      orig_f = fs.createWriteStream(orig);
+      updt_f = fs.createReadStream(updt);
+      updt_f.addListener("data", function(chunk) {
+        return orig_f.write(chunk);
       });
-    }
-  }).listen(port);
-};
-start_refresh_server = function(port, wpath) {
-  var io, watch;
-  watch = function(dir, fn) {
-    var walker;
-    walker = walk.walk(dir, {});
-    walker.on("names", function(root, nodeNames) {
-      return nodeNames.sort(function(a, b) {
-        if (a === b) {
-          return 0;
-        } else {
-          if (a > b) {
-            return 1;
-          } else {
-            return -1;
-          }
-        }
+      return updt_f.addListener("close", function() {
+        return orig_f.end();
       });
-    });
-    walker.on("file", function(root, fileStats, next) {
-      fs.watchFile(path.join(root, fileStats.name), fn);
-      return next();
-    });
-    walker.on("directories", function(root, dirStatsArray, next) {
-      return next();
-    });
-    walker.on("errors", function(root, nodeStatsArray, next) {
-      return next();
-    });
-    return walker.on("end", function() {
-      return log("all done");
-    });
-  };
-  io = socket.listen(port);
-  global.cb = null;
-  io.sockets.on('connection', function(socket) {
-    log('registering refresh socket');
-    return global.cb = function() {
-      log('called by watcher');
-      return socket.emit('refresh');
     };
-  });
-  return watch(wpath, function() {
-    log('path: ' + wpath);
-    return typeof cb === "function" ? cb() : void 0;
-  });
-};
+    this.server = http.createServer(function(req, res) {
+      var form;
+      if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
+        form = new formidable.IncomingForm();
+        return form.parse(req, function(err, fields, files) {
+          res.writeHead(204, {
+            'content-type': 'text/plain',
+            'Access-Control-Allow-Origin': '*'
+          });
+          update_local_file(path.join(cwd, fields['path']), files['blob']['path']);
+          return res.end();
+        });
+      }
+    });
+    this.server.listen(port);
+    log('upload: started upload server');
+  }
+  UploadServer.prototype.close = function() {
+    this.server.close();
+    return log('upload: closed upload server');
+  };
+  return UploadServer;
+})();
+RefreshServer = (function() {
+  function RefreshServer(port, wpath) {
+    var io, watch;
+    watch = function(dir, fn) {
+      var walker;
+      walker = walk.walk(dir, {});
+      walker.on("names", function(root, nodeNames) {
+        return nodeNames.sort(function(a, b) {
+          if (a === b) {
+            return 0;
+          } else {
+            if (a > b) {
+              return 1;
+            } else {
+              return -1;
+            }
+          }
+        });
+      });
+      walker.on("file", function(root, fileStats, next) {
+        fs.watchFile(path.join(root, fileStats.name), fn);
+        return next();
+      });
+      walker.on("directories", function(root, dirStatsArray, next) {
+        return next();
+      });
+      walker.on("errors", function(root, nodeStatsArray, next) {
+        return next();
+      });
+      return walker.on("end", function() {
+        return log("refresh: closed refresh server");
+      });
+    };
+    io = socket.listen(port);
+    global.cb = null;
+    io.sockets.on('connection', function(socket) {
+      log('refresh: registering refresh socket');
+      return global.cb = function() {
+        log('refresh: refresh request');
+        return socket.emit('refresh');
+      };
+    });
+    watch(wpath, function() {
+      log('path: ' + wpath);
+      return typeof cb === "function" ? cb() : void 0;
+    });
+  }
+  return RefreshServer;
+})();
 exec_cmd = function(cmd, opts) {
   var child;
   log('cmd: ', cmd, ' opts: ', opts);
@@ -114,11 +126,11 @@ exec_cmd = function(cmd, opts) {
     return process.stderr.write(data);
   });
   return child.on('exit', function() {
-    return log('exiting');
+    return process.kill(process.pid, 'SIGINT');
   });
 };
 handle_chromium = function(argv) {
-  var cb, chromium_path, opts;
+  var cb, opts;
   opts = [];
   if (argv.i != null) {
     opts.push("-f");
@@ -129,19 +141,18 @@ handle_chromium = function(argv) {
   if (argv.l != null) {
     opts.push("-t");
   }
-  chromium_path = path.join(__dirname, "devsync-chromium");
-  log('options: ', chromium_path, opts);
+  log('options: ', opts);
   return exec_cmd("devsync-chromium", opts);
 };
 module.exports.devsync = function() {
-  var argv, cwd, watch_path;
+  var argv, cwd, ups, watch_path;
   cwd = fs.realpathSync(process.cwd());
   argv = optimist.argv;
   watch_path = argv._ ? fs.realpathSync(argv._) : cwd;
   global.debug = argv.v != null;
   if (!(argv.r != null)) {
-    start_upload_server(9888, cwd);
-    start_refresh_server(9889, watch_path);
+    ups = new UploadServer(9888, cwd);
+    new RefreshServer(9889, watch_path);
   }
   process.on('uncaughtException', function() {
     log('uncaughtException: Blame it on ciju.ch3rian@gmail.com, some details would be appreciated.');
@@ -151,10 +162,12 @@ module.exports.devsync = function() {
     log('termination signal. do stuff');
     return process.exit();
   });
-  process.on('SIGINT', function() {
-    log('---------xxxxxxxxx');
+  process.on('SIGINT', __bind(function() {
+    if (ups != null) {
+      ups.close();
+    }
     return process.exit();
-  });
+  }, this));
   log('setting up chromium: please wait');
   return handle_chromium(argv);
 };
